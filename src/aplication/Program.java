360bb82056fea31bf8d3a9f7119944789a861b4e
Diego Brito <1992diego1992@gmail.com>
2020-11-20 15:40:29 -0300

    Seller Entities

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,13 +1,18 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Computador");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(1, "Bob", new Date(), 1000.0, obj);
+		System.out.println(seller);
 
 	}
 
